75422c760a85281f5ee7c9135069a0c36477569c
Miguel Ribeiro <miguel.rib.20@gmail.com>
2018-02-12 20:02:25 +0000

    WIP get max/min

--- src/data/mapMaker.js
+++ src/data/mapMaker.js
@@ -25,6 +25,37 @@ module.exports = {
         selectedRoutes.forEach((route) => {
             let data = route.routesVehicles;
 
+            // move this to createMap
+            let minLat = 1000;
+            let maxLat = -1000;
+            let minLon = 1000;
+            let maxLon = -1000; 
+
+            d3.json('./json/streets.json', (err, data) => {
+                data.features.forEach((route) => {
+                    route.geometry.coordinates.forEach((coordinate) => {
+                        if (coordinate[0] > maxLon) {
+                            maxLon = coordinate[0];
+                        }
+
+                        if (coordinate[0] < minLon) {
+                            minLon = coordinate[0];
+                        }
+
+                        if (coordinate[1] > maxLat) {
+                            maxLat = coordinate[1];
+                        }
+
+                        if (coordinate[1] < minLat) {
+                            minLat = coordinate[1];
+                        }
+                    });
+                    // console.log('streets', route.geometry.coordinates);
+                });
+
+                console.log('fim', minLat, minLon, maxLat, maxLon);
+            });
+
             console.log('mapMaker', data);
         });
     }
